44003b68586404ab01da9e10377c9f62492dac81
yamashita-a420 <ayanonagai420@gmail.com>
2022-05-02 09:49:34 +0900

    [Add] ログイン機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#top'
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: "user_sessions#create"
+  post 'logout', to: 'user_sessions#destroy'
+
   resources :users, only: %i[new create]
 end

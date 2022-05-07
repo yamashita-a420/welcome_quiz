aab4309670eb61f8a9ae09e545c3cb9ef848444b
yamashita-a420 <ayanonagai420@gmail.com>
2022-05-07 14:29:29 +0900

    [Update] 質問モデルにenumを追加、関連の修正

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,9 @@
 class Question < ApplicationRecord
   mount_uploader :photo, PhotoUploader
   belongs_to :user
-  has_many :choice, dependent: :destroy
+  has_many :choices, dependent: :destroy
+
+  enum role: { incorrect: 0, correct: 1 }
 
   validates :content, presence: true, length: { maximum: 30 }
   validates :comment, presence: true, length: { maximum: 30 }

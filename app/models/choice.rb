0f2dc2b076d128ba04e162ac82493683ebf559f1
yamashita-a420 <ayanonagai420@gmail.com>
2022-05-07 15:45:38 +0900

    [Update] enumを追加

--- app/models/choice.rb
+++ app/models/choice.rb
@@ -2,5 +2,7 @@ class Choice < ApplicationRecord
   belongs_to :user
   belongs_to :question
   
+  enum correct_answer: { incorrect: 0, correct: 1}
+
   validates :content, presence: true, length: { maximum: 30 }
 end

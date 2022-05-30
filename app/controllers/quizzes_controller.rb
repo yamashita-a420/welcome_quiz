7663a7b401a25f26dbb2a387d006748684a018c3
yamashita-a420 <ayanonagai420@gmail.com>
2022-05-30 11:36:08 +0900

    Update: クイズのトップ画面表示機能と、クイズ表示機能を追加

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,4 +1,12 @@
 class QuizzesController < ApplicationController
+  def index; end
+
   def show
+    @questions = Question.all.sample(3)
+    loop do
+      break if @questions.empty?
+      @question = @questions.pop
+      @choices = @question.choices.order(created_at: :desc)
+    end
   end
 end

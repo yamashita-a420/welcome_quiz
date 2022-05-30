1d286e93976d51ca8daee76b5b432b89c4364fcb
yamashita-a420 <ayanonagai420@gmail.com>
2022-05-30 12:34:05 +0900

    Update: index・showアクションの質問を共通化

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,12 +1,20 @@
 class QuizzesController < ApplicationController
+  before_action :set_questions, only: %i[index show]
+
   def index; end
 
   def show
-    @questions = Question.all.sample(3)
     loop do
       break if @questions.empty?
       @question = @questions.pop
       @choices = @question.choices.order(created_at: :desc)
     end
+  end
+
+  private
+
+  def set_questions
+    @questions = Question.all.sample(3)
+    @question = @questions.pop
   end
 end

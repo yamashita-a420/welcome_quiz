4330ba5056c8da486b166c45ce708d435bf50281
yamashita-a420 <ayanonagai420@gmail.com>
2022-06-06 16:48:39 +0900

    Fix: テーブル名をguestsからmessagesへ修正

--- app/controllers/third_questions_controller.rb
+++ app/controllers/third_questions_controller.rb
@@ -1,4 +1,8 @@
 class ThirdQuestionsController < ApplicationController
   def show
+    @third_question = Question.find(params[:format])
+    @choices = @third_question.choices.order(created_at: :desc)
+    @first_question = Question.find(params[:question][:first_question])
+    @second_question = Question.find(params[:question][:second_question])
   end
 end

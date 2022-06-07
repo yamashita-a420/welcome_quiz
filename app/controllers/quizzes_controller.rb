5e5fcf52f88fb4a3a1ec2b03f4c1c24cdbef1535
yamashita-a420 <ayanonagai420@gmail.com>
2022-06-07 20:54:47 +0900

    Update: ログインをスキップする設定を追加

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,4 +1,5 @@
 class QuizzesController < ApplicationController
+  skip_before_action :require_login, only: %i[index]
 
   def index
     @first_question, @second_question, @third_question = Question.all.sample(3)
--- app/controllers/second_questions_controller.rb
+++ app/controllers/second_questions_controller.rb
@@ -1,4 +1,6 @@
 class SecondQuestionsController < ApplicationController
+  skip_before_action :require_login, only: %i[show]
+
   def show
     @second_question = Question.find(params[:format])
     @choices = @second_question.choices.order(created_at: :desc)

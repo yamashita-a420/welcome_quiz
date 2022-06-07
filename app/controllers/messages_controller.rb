0261e9984705b59cc0c22057bfaf0d8ddda8143f
yamashita-a420 <ayanonagai420@gmail.com>
2022-06-08 00:22:08 +0900

    Uodate: 正解数を表示する機能を実装中

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,10 +7,24 @@ class MessagesController < ApplicationController
 
   def new
     @message = Message.new
+    # 表示するquestion
     @first_question = Question.find(params[:question][:first_question])
     @second_question = Question.find(params[:question][:second_question])
     @third_question = Question.find(params[:format])
-    @choices = @first_question.choices.order(created_at: :desc)
+    # 回答したchoiceの情報
+    @first_choice = Choice.find(params[:question][:first_choice])
+    @second_choice = Choice.find(params[:question][:second_choice])
+    #thirdがsecondと同じになってる？
+    @third_choice = Choice.find(params[:question][:third_choice])
+    #answer = [@first_choice, @second_choice, @third_choice]
+    # 正解数
+    #@score = scoring([ @first_choice, @second_choice, @third_choice ])
+    #@score = Choice.scoring(answer)
+    #@score = @first_choice.scoring + @second_choice.scoring + @third_choice.scoring
+    binding.pry
+    score = @first_choice.scoring(@first_choice)
+    binding.pry
+    @score = @first_choice.scoring(@first_choice) + @second_choice.scoring(@second_choice) + @third_choice.scoring(@third_choice)
   end
 
   def create
--- app/models/choice.rb
+++ app/models/choice.rb
@@ -1,8 +1,26 @@
 class Choice < ApplicationRecord
   belongs_to :user
   belongs_to :question
-  
+
   enum correct_answer: { incorrect: 0, correct: 1}
 
   validates :content, presence: true, length: { maximum: 30 }
+
+  def scoring(choice)
+    score = 0
+    score += 1 if choice.correct_answer == "correct"
+    #binding.pry
+    puts score.to_i
+  end
+
+  #def self.scoring(*choices)
+  #  score = 0
+  #  choices.each do |choice|
+  #    #binding.pry
+  #    if choice.correct_answer == 1
+  #      #binding.pry
+  #      score += 1
+  #    end
+  #  end
+  #end
 end

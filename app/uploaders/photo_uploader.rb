e2396e170cb9b1677e6fb50945240635206a130c
yamashita-a420 <ayanonagai420@gmail.com>
2022-06-12 16:29:24 +0900

    Update: gem fog-awsとfigaroを追加して、本番環境のストレージをS3に変更

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,5 +1,9 @@
 class PhotoUploader < CarrierWave::Uploader::Base
-  storage :file
+  if Rails.env.production?
+    storage :fog # 本番環境のみ
+  else
+    storage :file # 本番環境以外
+  end
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"

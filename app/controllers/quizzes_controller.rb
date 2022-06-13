0aeb7158e099fad873ae0f0ed7992d11f3cc14a8
yamashita-a420 <ayanonagai420@gmail.com>
2022-06-13 17:34:56 +0900

    Update: クイズtopのレイアウトなど修正

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,5 +1,6 @@
 class QuizzesController < ApplicationController
   skip_before_action :require_login, only: %i[index]
+  layout 'layouts/quiz_top'
 
   def index
     @first_question, @second_question, @third_question = Question.all.sample(3)

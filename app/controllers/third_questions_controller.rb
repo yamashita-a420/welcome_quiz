8eecbeb92ff508de18b9b78f571fa65e8a3fff65
yamashita-a420 <ayanonagai420@gmail.com>
2022-06-15 09:55:07 +0900

    Fix: ルーティング変更に伴うパスの修正・2,3問目が同じになるバグを解消

--- app/controllers/third_questions_controller.rb
+++ app/controllers/third_questions_controller.rb
@@ -7,10 +7,11 @@ class ThirdQuestionsController < ApplicationController
     @third_question = Question.find(params[:id])
     @choices = @third_question.choices.order(created_at: :desc)
     # 設定したquestionの情報
-    @first_question = Question.find(params[:question][:first_question])
-    @second_question = Question.find(params[:question][:second_question])
+    @first_question = Question.find(params[:first_question])
+    @second_question = Question.find(params[:second_question])
     # 回答したchoiceの情報
-    @first_choice = Choice.find(params[:question][:first_choice])
-    @second_choice = Choice.find(params[:question][:second_choice])
+    @first_choice = Choice.find(params[:first_choice])
+    @second_choice = Choice.find(params[:second_choice])
+    binding.pry
   end
 end

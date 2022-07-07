acfdeacd175f76a8e7e8c380765ff6199e830feb
yamashita-a420 <ayanonagai420@gmail.com>
2022-07-07 15:40:42 +0900

    Update: messagesのidをuuidに変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_07_063529) do
+ActiveRecord::Schema.define(version: 2022_07_07_063854) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 2022_07_07_063529) do
     t.index ["user_id"], name: "index_choices_on_user_id"
   end
 
-  create_table "messages", force: :cascade do |t|
+  create_table "messages", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name", null: false
     t.string "body", default: "クイズ楽しかったよ！", null: false
     t.datetime "created_at", precision: 6, null: false

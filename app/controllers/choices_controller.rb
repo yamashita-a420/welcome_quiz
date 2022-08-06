f12cadb4873599099861dd5858e19ef519ccaa24
yamashita-a420 <ayanonagai420@gmail.com>
2022-08-06 21:18:49 +0900

    Fix: questionsコントローラで動作するため、不要になったアクションを削除

--- app/controllers/choices_controller.rb
+++ app/controllers/choices_controller.rb
@@ -1,36 +1,14 @@
 class ChoicesController < ApplicationController
-  before_action :set_choice, only: %i[ edit update destroy ]
+  before_action :set_choice, only: %i[ edit ]
 
   def new; end
 
-  def create
-    @choice = @question.choices.build(choice_params)
-    @choice.save
-  end
-
   def edit; end
-
-  def update
-    if @choice.update(choice_params)
-      redirect_to choice_url(@choice), notice: "Choice was successfully updated."
-    else
-      render :edit, status: :unprocessable_entity
-    end
-  end
-
-  def destroy
-    @choice.destroy
-    redirect_to choices_url, notice: "Choice was successfully destroyed."
-  end
 
   private
 
   def set_choice
     @question = Question.find(params[:id])
     @choice = @question.choices.find(params[:id])
-  end
-
-  def choice_params
-    params.require(:choice).permit(:content, :correct_answer).merge(question_id: params[:question_id])
   end
 end

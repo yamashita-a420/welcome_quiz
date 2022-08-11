ae5a0e9ef1f44f7acf4f322c88d647a94832ee73
yamashita-a420 <ayanonagai420@gmail.com>
2022-08-11 09:12:31 +0900

    Fix: 簡潔な記述に修正

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -5,34 +5,28 @@ class PasswordResetsController < ApplicationController
 
   def create
     @user = User.find_by_email(params[:email])
-    @user.deliver_reset_password_instructions! if @user
-    redirect_to(root_path, :notice => 'Instructions have been sent to your email.')
+    @user&.deliver_reset_password_instructions!
+    redirect_to login_path, success: t('.success')
   end
 
   def edit
     @token = params[:id]
     @user = User.load_from_reset_password_token(params[:id])
-
-    if @user.blank?
-      not_authenticated
-      return
-    end
+    not_authenticated if @user.blank?
   end
 
   def update
     @token = params[:id]
-    @user = User.load_from_reset_password_token(params[:id])
+    @user = User.load_from_reset_password_token(@token)
 
-    if @user.blank?
-      not_authenticated
-      return
-    end
+    return not_authenticated if @user.blank?
 
     @user.password_confirmation = params[:user][:password_confirmation]
     if @user.change_password(params[:user][:password])
-      redirect_to(root_path, :notice => 'Password was successfully updated.')
+      redirect_to login_path, success: t('.success')
     else
-      render :action => "edit"
+      flash.now[:danger] = t('.fail')
+      render :edit
     end
   end
 end

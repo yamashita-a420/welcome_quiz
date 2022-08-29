c5374aab812605f18b69bd0b3bd744db0c52cdaa
yamashita-a420 <ayanonagai420@gmail.com>
2022-08-29 18:14:19 +0900

    Update: QRcodeをpng形式に変更。
    pngで表示するが、保存はしない。

--- app/helpers/qrcodes_helper.rb
+++ app/helpers/qrcodes_helper.rb
@@ -1,2 +1,8 @@
 module QrcodesHelper
+  require 'chunky_png'
+
+  def qrcode_tag(content, options = {})
+    qr = ::RQRCode::QRCode.new(content, options)
+    return ChunkyPNG::Image.from_datastream(qr.as_png.resize(500,500).to_datastream).to_data_url
+  end
 end

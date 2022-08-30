d817918d8c5a35c125736167f2941142617c3b30
yamashita-a420 <ayanonagai420@gmail.com>
2022-08-30 09:31:39 +0900

    Update: QRコードの余白修正・使い方追加

--- app/helpers/qrcodes_helper.rb
+++ app/helpers/qrcodes_helper.rb
@@ -2,7 +2,7 @@ module QrcodesHelper
   require 'chunky_png'
 
   def qrcode_tag(content, options = {})
-    qr = ::RQRCode::QRCode.new(content, options)
-    return ChunkyPNG::Image.from_datastream(qr.as_png.resize(500,500).to_datastream).to_data_url
+    qr = ::RQRCode::QRCode.new(content)
+    return ChunkyPNG::Image.from_datastream(qr.as_png(border_modules: 4).resize(300,300).to_datastream).to_data_url
   end
 end

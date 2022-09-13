18b2a4b182373ecfa090583d8989a12309357025
yamashita-a420 <ayanonagai420@gmail.com>
2022-09-14 00:16:37 +0900

    ref: メッセージコントローラ中心にコード量を削減

--- app/controllers/take_quizzes_controller.rb
+++ app/controllers/take_quizzes_controller.rb
@@ -1,7 +1,7 @@
 class TakeQuizzesController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @question_1, @question_2, @question_3 = @user.questions.sample(3)
-    @choices_1, @choices_2, @choices_3 = [@question_1, @question_2, @question_3].map(&:choices).map{|x| x.pluck(:content)}
+    @quiz_1, @quiz_2, @quiz_3 = @user.questions.sample(3)
+    @choices_1, @choices_2, @choices_3 = [@quiz_1, @quiz_2, @quiz_3].map(&:choices).map{|x| x.pluck(:content)}
   end
 end

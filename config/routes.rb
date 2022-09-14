13b4ef29c056b92fb10046d5e9a6efe67d769b5c
yamashita-a420 <ayanonagai420@gmail.com>
2022-09-14 19:40:41 +0900

    Delete: 不要なコントローラ一式を削除

--- config/routes.rb
+++ config/routes.rb
@@ -12,9 +12,6 @@ Rails.application.routes.draw do
   resources :users, only: %i[new create destroy] do
     resources :quizzes, only: %i[index]
     resources :take_quizzes, only: %i[index]
-    post 'first_question', to: 'first_questions#show'
-    post 'second_question', to: 'second_questions#show'
-    post 'third_question', to: 'third_questions#show'
     post 'result', to: 'messages#new'
     post 'result_message', to: 'messages#create'
     get 'result_message_sent', to: 'messages#show'

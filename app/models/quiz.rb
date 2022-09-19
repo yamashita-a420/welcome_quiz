65d68f15beed33b4e11aac22835f4f289d542936
yamashita-a420 <ayanonagai420@gmail.com>
2022-09-19 23:44:57 +0900

    Delete: 不要なコントローラ一式を削除

--- app/models/quiz.rb
+++
@@ -1,9 +0,0 @@
-class Quiz < ApplicationRecord
-  mount_uploader :photo, QuizTopUploader
-  belongs_to :user
-  has_many :quiz_tops, dependent: :destroy
-  accepts_nested_attributes_for :quiz_tops, allow_destroy: true
-
-  validates :title, presence: true
-  validates :explain, presence: true
-end

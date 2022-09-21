d30ebee62febc2b40ba459598f0bbf1b0f635750
yamashita-a420 <ayanonagai420@gmail.com>
2022-09-21 18:43:37 +0900

    Fix: 本番環境のURLを独自ドメインへ修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,7 +118,7 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.action_mailer.default_url_options = { :host => 'http://welcome-quiz-maker.herokuapp.com/' }
+  config.action_mailer.default_url_options = { :host => 'http://welcome-quiz-maker.com/' }
 
   config.action_mailer.delivery_method = :smtp
 

7398315f6468e5b908134802ea17d420feba34f0
yamashita-a420 <ayanonagai420@gmail.com>
2022-09-21 23:23:15 +0900

    Update: ユーザー登録後スタート画面が保存されるようコールバックを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,20 @@ class User < ApplicationRecord
   has_many :messages, dependent: :destroy
   has_one :quiz_top, dependent: :destroy
 
+  validates :name, presence: true
+  validates :email, uniqueness: true, presence: true
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
   validates :reset_password_token, uniqueness: true, allow_nil: true
 
-  validates :email, uniqueness: true, presence: true
-  validates :name, presence: true
-
   enum role: { general: 0, admin: 1 }
+
+  after_validation :set_quiz_top, on: [ :create ]
+
+  private
+
+  def set_quiz_top
+    self.quiz_top = QuizTop.create
+  end
 end
